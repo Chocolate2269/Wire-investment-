8dc0ea358012ff28ff40bc22c001e71f21c2295c
Chocolate2269 <somtooo921@gmail.com>
2026-08-19 14:31:53 +0100

    Update configure.js

--- configure.js
+++ configure.js
@@ -1,1 +1,3 @@
-
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2'
+export const supabase = createClient('YOUR_SUPABASE_URL', 'YOUR_SUPABASE_ANON_KEY') 
+export const RESEND_KEY = 'YOUR_RESEND_KEY'
